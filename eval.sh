e3b551637c2772fb6077f73b619a2c61e6d22828
jmy <124435499+MingyuJ666@users.noreply.github.com>
2024-10-21 17:01:40 -0400

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -7,7 +7,7 @@ EFFICIENT=lora+prompt-tuning
 STEP_TYPE=memory
 CUDA_VISIBLE_DEVICES=1 python eval.py \
     --base_model_name_or_path $MODEL \
-    --hf_hub_token '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' \
+    --hf_hub_token '' \
     --model_name_or_path  /common/home/mj939/planning_tokens/checkpoints/meta-llama/Llama-2-7b-chat-hf/stratgeqa_agent/step_type=memory-2-3-efficient=lora+prompt-tuning-lr=2e-4-soft-prompt=True/checkpoint-1000 \
     --add_soft_prompts $ADD_SOFT_PROMPT\
     --num_general_prefix_tokens $N_PREFIX \
